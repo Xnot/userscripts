ab52086fbfb8808021ec3872e2bcb370450b2c5d
Lucas Pratas <lucaspratas@hotmail.com>
2018-02-10 09:45:21 -0200

    added update field

--- mangadex-massuploader.meta.js
+++ mangadex-massuploader.meta.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Mangadex (shitty) Mass Uloader
 // @namespace    https://github.com/LucasPratas/userscripts
-// @version      1.2
+// @version      1.3
 // @icon         https://mangadex.com/favicon.ico
 // @description  try to get green!
 // @author       Xnot
+// @updateURL    https://github.com/LucasPratas/userscripts/raw/master/mangadex-massuploader.meta.js
+// @downloadURL  https://github.com/LucasPratas/userscripts/raw/master/mangadex-massuploader.user.js
 // @match        https://mangadex.com/upload/*
 // @grant        none
 // ==/UserScript==
--- mangadex-massuploader.user.js
+++ mangadex-massuploader.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         shitty mangadex mass upload attempt
+// @name         Mangadex (shitty) Mass Uloader
 // @namespace    https://github.com/LucasPratas/userscripts
-// @version      1.2
+// @version      1.3
 // @icon         https://mangadex.com/favicon.ico
 // @description  try to get green!
 // @author       Xnot
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://github.com/LucasPratas/userscripts/raw/master/mangadex-massuploader.meta.js
+// @downloadURL  https://github.com/LucasPratas/userscripts/raw/master/mangadex-massuploader.user.js
 // @match        https://mangadex.com/upload/*
 // @grant        none
 // ==/UserScript==
