d2f2565c9f3f614f3d3259f1826d87e5243db0c8
Lucas Pratas <lucaspratas@hotmail.com>
2018-02-10 09:39:21 -0200

    added meta shit and renamed

---
+++ mangadex-massuploader.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Mangadex (shitty) Mass Uloader
+// @namespace    https://github.com/LucasPratas/userscripts
+// @version      1.2
+// @icon         https://mangadex.com/favicon.ico
+// @description  try to get green!
+// @author       Xnot
+// @match        https://mangadex.com/upload/*
+// @grant        none
+// ==/UserScript==
