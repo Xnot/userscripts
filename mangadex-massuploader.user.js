0c25add055a80829f334f8fd4144f8b609d84851
Lucas Pratas <lucaspratas@hotmail.com>
2018-02-13 17:39:20 -0200

    forgot to update version
    that's what happens when rushing fixes

--- mangadex-massuploader.user.js
+++ mangadex-massuploader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mangadex (shitty) Mass Uploader
 // @namespace    https://github.com/LucasPratas/userscripts
-// @version      1.62
+// @version      1.63
 // @icon         https://mangadex.com/favicon.ico
 // @description  try to get green!
 // @author       Xnot
