63fd41fd625cb1cae403fc540790f891a0fddb87
Lucas Pratas <lucaspratas@hotmail.com>
2018-03-20 01:03:24 -0300

    Fixed update url and updated meta

--- mangadex-massuploader.meta.js
+++ mangadex-massuploader.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name         Mangadex (shitty) Mass Uploader
+// @name         Mangadex Mass Uploader
 // @namespace    https://github.com/LucasPratas/userscripts
-// @version      1.86
-// @icon         https://mangadex.com/favicon.ico
+// @version      1.91
+// @icon         https://mangadex.org/favicon.ico
 // @description  try to get green!
 // @author       Xnot
-// @updateURL    https://github.com/LucasPratas/userscripts/raw/master/mangadex-massuploader.user.js
-// @downloadURL  https://github.com/LucasPratas/userscripts/raw/master/mangadex-massuploader.user.js
+// @updateURL    https://raw.githubusercontent.com/LucasPratas/userscripts/master/mangadex-massuploader.meta.js
+// @downloadURL  https://raw.githubusercontent.com/LucasPratas/userscripts/master/mangadex-massuploader.user.js
 // @match        https://mangadex.org/upload/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @grant        none
 // ==/UserScript==
